71b37c4f8b7372de0ce8a39e7d54e38c555f79e6
Edison Neves Soares <86014556+edineves@users.noreply.github.com>
2022-01-10 21:04:44 -0300

    add

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,5 +1,16 @@
-novaTarefa.addEventListener('click', () => {
-    const input = document.querySelector(['data-form-input'])
+const criarTarefa = (evento) => {
+
+    evento.preventDefault()
+
+    const input = document.querySelector('[data-form-input]')
     const valor = input.value
-    console.log(valor)
-})
+
+    const tarefa = document.querySelector('[data-task]')
+    const conteudo = `<p class="content">${valor}<\p>`
+
+    tarefa.innerHTML = conteudo
+
+    input.value = " "
+}
+
+const novaTarefa = document.querySelector('[data-form-button]')
